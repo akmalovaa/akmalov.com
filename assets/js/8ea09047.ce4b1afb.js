18a446c095c8b2955edd7e16d490ced32417df73
akmalovaa <akmalovaa@users.noreply.github.com>
2025-07-27 21:11:04 +0000

    deploy: 4ca43200710d4636de5278a92d4abddc264ddddd

---
+++ assets/js/8ea09047.ce4b1afb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkakmalov_com=self.webpackChunkakmalov_com||[]).push([[9385],{600:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":16,"previousPage":"/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
